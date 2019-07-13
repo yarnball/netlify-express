e6046c79804780466b07fa4df86ce80f3e3dd4fa
Yarnball <yconomos@gmail.com>
2019-07-13 23:32:02 +1000

    test(cookies): test netlify handles secure cookies

--- express/server.js
+++ express/server.js
@@ -2,19 +2,15 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
+const cors = require('cors')({credentials: true, origin:true})
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
+app.use(cors)
+app.get('/', (req, res) => {
+	res.status(200).send('Server running at!')
+})
+app.get('/log', (req, res) => {
+	console.log(req)
+	res.status(200).json(req.headers)
+})
 module.exports = app;
 module.exports.handler = serverless(app);
